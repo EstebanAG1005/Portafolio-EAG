f96ced8969d80e5c3df3b618a16591edd443dad5
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2022-06-02 22:28:56 -0600

    cambios

--- src/components/Proyectos.js
+++ src/components/Proyectos.js
@@ -16,16 +16,12 @@ const Proyectos = () => {
                         <img src={Memoria} alt='user1'/>
                         <h1 className='Titulo'>Proyecto Memoria</h1>
                         <p>Este proyecto fue desarrollado con las tecnologías de HTML y CSS, este proyecto es un juego de Memoria. Fue uno de los primeros proyectos desarrollados por mi persona.</p>
-                        <p><span>Desarrollado por</span></p>
-                        <p>Esteban Aldana Guerra</p>
                         <a className='button1' href='https://juanelcaballo.club/20591/Lab06/Memoria.html'>Ver Sitio</a>
                     </div>
                     <div className='card'>
                         <img src={Ele} alt='user1'/>
                         <h1 className='Titulo'>Proyecto Copa de Pagina Web</h1>
                         <p>Para este proyecto se desarrolló la copia de una página web, esta página se desarrolla principalmente con React, al igual que usando varias tecnologías cómo Babel, Webpack, etc.</p>
-                        <p><span>Desarrollado por</span></p>
-                        <p>Esteban Aldana Guera</p>
                         <a className='button1' href='https://elevationworship-copy-page.netlify.app/'>Ver Sitio</a>
                        
                     </div>
@@ -33,16 +29,12 @@ const Proyectos = () => {
                         <img src={Calc} alt='user1'/>
                         <h1 className='Titulo'>Proyecto Calculadora</h1>
                         <p>Este proyecto fue desarrollado con la herramienta de Create React App con la cual se crea un ambiente de trabajo de React, se trabajo en una calculadora funcional, al igual que fue estetica.</p>
-                        <p><span>Desarrollado por</span></p>
-                        <p>Esteban Aldana Guera</p>
                         <a className='button1' href='https://juanelcaballo.club/20591/build/index.html'>Ver Sitio</a>
                     </div>
                     <div className='card'>
                         <img src={lab} alt='user1'/>
                         <h1 className='Titulo'>Proyecto Laberinto</h1>
                         <p>Este proyecto fue desarrollado con elementos de React, al igual que con varias herramientas como babel y Webpack, Este es un juego de un laberiento el cual puede ser expandido segun escoga el usuario.</p>
-                        <p><span>Desarrollado por</span></p>
-                        <p>Esteban Aldana Guera</p>
                         <a className='button1' href='https://juanelcaballo.club/20591/Laberinto/dist/index.html'>Ver Sitio</a>
                     </div>
                     
